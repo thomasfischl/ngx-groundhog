17e90ee47eb84f387bf8688a45167c5843082a4c
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-01-22 10:05:07 +0100

    chore(tools): removed examples package

--- tools/gulp/packages.ts
+++ tools/gulp/packages.ts
@@ -7,7 +7,7 @@ import {BuildPackage, buildConfig} from 'ngx-groundhog-build-tools';
 import {join} from 'path';
 
 export const groundhogPackage = new BuildPackage('ngx-groundhog');
-export const examplesPackage = new BuildPackage('ngx-groundhog-examples', [groundhogPackage]);
+// export const examplesPackage = new BuildPackage('ngx-groundhog-examples', [groundhogPackage]);
 
 // The groundhog package re-exports its secondary entry-points at the root so that all of the
 // components can still be imported through `@dynatrace/ngx-groundhog`.
