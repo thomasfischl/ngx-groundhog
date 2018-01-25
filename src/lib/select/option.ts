2b982903b5359e273a772ffa75051cea35f5557c
Thomas Pink <thomas.pink@me.com>
2018-01-25 14:56:19 +0100

    feat(select): Finished implementing select + option logic, also added selection model

--- src/lib/select/option.ts
+++ src/lib/select/option.ts
@@ -4,8 +4,18 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Output,
-  EventEmitter
+  Input,
+  EventEmitter,
+  ElementRef,
 } from '@angular/core';
+
+/** Event object emitted by MatOption when selected or deselected. */
+export interface GhOptionSelectionChange {
+    /** Reference to the option that emitted the event. */
+    source: GhOption;
+    /** Whether the change in the option's value was a result of a user action. */
+    isUserInput: boolean;
+}
 
 @Component({
   selector: 'gh-option',
@@ -16,7 +26,7 @@ import {
   host: {
     'role': 'option',
     '[attr.aria-selected]': 'selected.toString()',
-    '(click)': '_toggle()',
+    '(click)': '_toggleViaInteraction()',
     'class': 'gh-option',
   },
   encapsulation: ViewEncapsulation.None,
@@ -26,36 +36,53 @@ import {
 export class GhOption {
   private _selected = false;
 
-  get selected() {
-    return this._selected;
+  get selected() { return this._selected; }
+
+  /** The displayed value of the option. */
+  get viewValue(): string {
+    return (this._element.nativeElement.textContent || '').trim();
   }
 
-  @Output()
-  onSelectionChange = new EventEmitter<boolean>();
 
-  constructor(private _changeDetectorRef: ChangeDetectorRef) {
+  /** The form value of the option. */
+  @Input() value: any;
 
-  }
+  /** Event emitted when the option is selected or deselected. */
+  @Output() onSelectionChange = new EventEmitter<GhOptionSelectionChange>();
 
+  constructor (private _changeDetectorRef: ChangeDetectorRef,
+               private _element: ElementRef) { }
+
+  /** Selects the option. */
   select() {
     if (!this._selected) {
       this._selected = true;
       this._changeDetectorRef.markForCheck();
-      this.onSelectionChange.emit(true);
+      this._emitSelectionChangeEvent();
     }
   }
 
+  /** Deselects the option. */
   deselect() {
     if (this._selected) {
       this._selected = false;
       this._changeDetectorRef.markForCheck();
-      this.onSelectionChange.emit(false);
+      this._emitSelectionChangeEvent();
     }
   }
 
-  _toggle() {
+  /**
+   * Selects the option while indicating the selection came from the user. Used to
+   * determine if the select's view -> model callback should be invoked.
+   */
+  _toggleViaInteraction() {
     this._selected = !this._selected;
     this._changeDetectorRef.markForCheck();
-    this.onSelectionChange.emit(this._selected);
+    this._emitSelectionChangeEvent(true);
+  }
+
+  /** Emits the selection change event. */
+  private _emitSelectionChangeEvent(isUserInput = false): void {
+    this.onSelectionChange.emit({source: this, isUserInput});
   }
 }
--- src/lib/select/select.ts
+++ src/lib/select/select.ts
@@ -8,20 +8,23 @@ import {
   ChangeDetectorRef,
   ContentChildren,
   QueryList,
-  AfterContentInit,
+  OnInit,
   OnDestroy,
+  AfterContentInit,
   NgZone,
 } from '@angular/core';
-import { NgClass } from '@angular/common';
-import { startWith } from 'rxjs/operators/startWith';
-import { takeUntil } from 'rxjs/operators/takeUntil';
-import { switchMap } from 'rxjs/operators/switchMap';
-import { take } from 'rxjs/operators/take';
-import { merge } from 'rxjs/observable/merge';
-import { defer } from 'rxjs/observable/defer';
-import { Observable } from 'rxjs/Observable';
-import { GhOption } from './option';
-import { Subject } from 'rxjs/Subject';
+import {NgClass} from '@angular/common';
+import {SelectionModel} from '@angular/cdk/collections';
+import {startWith} from 'rxjs/operators/startWith';
+import {takeUntil} from 'rxjs/operators/takeUntil';
+import {switchMap} from 'rxjs/operators/switchMap';
+import {filter} from 'rxjs/operators/filter';
+import {take} from 'rxjs/operators/take';
+import {merge} from 'rxjs/observable/merge';
+import {defer} from 'rxjs/observable/defer';
+import {Observable} from 'rxjs/Observable';
+import {GhOption, GhOptionSelectionChange} from './option';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   moduleId: module.id,
@@ -33,7 +36,7 @@ import { Subject } from 'rxjs/Subject';
   preserveWhitespaces: false,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GhSelect implements AfterContentInit, OnDestroy {
+export class GhSelect implements OnInit, AfterContentInit, OnDestroy {
 
   /** The placeholder displayed in the trigger of the select. */
   private _placeholder: string;
@@ -41,10 +44,13 @@ export class GhSelect implements AfterContentInit, OnDestroy {
   /** Whether or not the overlay panel is open. */
   private _panelOpen = false;
 
-  private _selectedValue: any;
+  private _value: any;
 
   /** Emits whenever the component is destroyed. */
   private _destroy = new Subject();
+
+  /** Deals with the selection logic. */
+  _selectionModel: SelectionModel<GhOption>;
 
   /** Classes to be passed to the select panel. Supports the same syntax as `ngClass`. */
   @Input() panelClass: string | Set<string> | string[] | {[key: string]: any};
@@ -56,27 +62,50 @@ export class GhSelect implements AfterContentInit, OnDestroy {
     this._placeholder = newplaceholder;
   }
 
+  /** Value of the select. */
+  @Input()
+  get value(): any { return this._value; }
+  set value(newValue: any) {
+    // Only set the new value if it differes from the old one
+    if (newValue !== this._value) {
+      this._value = newValue;
+    }
+  }
+
+  /** The currently selected option. */
+  get selected(): GhOption {
+    return this._selectionModel.selected[0];
+  }
+
+  /** Whether the select has a value. */
   get empty() {
-    return !this._selectedValue;
+    return !this._selectionModel || this._selectionModel.isEmpty();
   }
 
+  /** The value displayed in the trigger. */
   get triggerValue() {
-    return this._selectedValue ? this._selectedValue.label : 'No value selected';
+    return this.empty ? '' : this._selectionModel.selected[0].viewValue;
   }
 
+  /** Whether or not the overlay panel is open. */
   get panelOpen() {
     return this._panelOpen;
   }
 
-  @ViewChild('panel')
-  panel: ElementRef;
+  /** Panel containing the select options. */
+  @ViewChild('panel') panel: ElementRef;
 
-  @ContentChildren(GhOption, { descendants: true })
+  /** All of the defined select options. */
+  @ContentChildren(GhOption, {descendants: true})
   options: QueryList<GhOption>;
 
   /** Combined stream of all of the child options' change events. */
-  optionSelectionChanges: Observable<boolean> = defer(() => {
+  optionSelectionChanges: Observable<GhOptionSelectionChange> = defer(() => {
     if (this.options) {
+      // This will create an array of onSelectionChange Observables and
+      // then combine/merge them into one so we can later easily subscribe
+      // to all at once and get the event (the option plus isUserEvent flag)
+      // of the triggered option
       return merge(...this.options.map(option => option.onSelectionChange));
     }
 
@@ -85,53 +114,104 @@ export class GhSelect implements AfterContentInit, OnDestroy {
       .pipe(take(1), switchMap(() => this.optionSelectionChanges));
   });
 
-  // constructor
-  constructor(private _changeDetectionRef: ChangeDetectorRef, private _ngZone: NgZone ) {
+  constructor(private _changeDetectiorRef: ChangeDetectorRef, private _ngZone: NgZone) { }
+
+  /**
+   * Hook that triggers after the component has been initialized
+   * and the first change detection has run.
+   */
+  ngOnInit() {
+    this._selectionModel = new SelectionModel<GhOption>(false, undefined, false);
   }
 
+  /** Hook that triggers when ng-content and all sub components (the options) are initialized. */
   ngAfterContentInit(): void {
+    // After the ng-content has been initialized we can start listening on
+    // the options query list for changes (new options get added, removed,...)
     this.options.changes
       .pipe(startWith(null), takeUntil(this._destroy))
+      // Everytime options change, we need to reset (resubscribe on their events, ...)
       .subscribe(() => this._resetOptions());
   }
 
+  /** Hook that trigger right before the component will be destroyed. */
   ngOnDestroy(): void {
     this._destroy.next();
     this._destroy.complete();
   }
 
-  /**
-   * Opens the panel
-   */
+  /** Opens the panel */
   open() {
     if (!this._panelOpen) {
       this._panelOpen = true;
-      this._changeDetectionRef.markForCheck();
+      this._changeDetectiorRef.markForCheck();
     }
   }
 
-  /**
-   * Closes the panel
-   */
+  /** Closes the panel */
   close() {
     if (this._panelOpen) {
       this._panelOpen = false;
-      this._changeDetectionRef.markForCheck();
+      this._changeDetectiorRef.markForCheck();
     }
   }
 
-  /**
-   * Toggles the panel
-  */
+  /** Toggles the panel */
   toggle() {
     this._panelOpen ? this.close() : this.open();
   }
 
+  /** Drops current option subscriptions and resets from scratch. */
   _resetOptions() {
     this.optionSelectionChanges
-      .pipe(takeUntil(merge(this._destroy, this.options.changes)))
-      .subscribe((evt) => {
-        console.log(evt);
+      .pipe(
+        // Stop listening when the component will be destroyed
+        // or the options have changed (new ones added or removed)
+        takeUntil(merge(this._destroy, this.options.changes)),
+        // We only need user events.
+        // The other ones are triggered to select/deselect on GhSelect level
+        filter(event => event.isUserInput))
+      .subscribe(evt => {
+        // Note: The event.source is the selected GhOption
+        this._onSelect(evt.source);
+
+        if (this._panelOpen) {
+          this.close();
+        }
       });
+  }
+
+  /** Invoked when an option is clicked. */
+  private _onSelect(option: GhOption): void {
+    const wasSelected = this._selectionModel.isSelected(option);
+
+    // Deselect all other options (than the one we just selected)
+    // and clear all options from our selectionModel
+    this._clearSelection(option);
+    this._selectionModel.select(option);
+
+    // Only propagate changes if the option has really changed.
+    // We do not want change events triggerd if the same option
+    // got selected twice in a row.
+    if (wasSelected !== this._selectionModel.isSelected(option)) {
+      this._propagateChanges();
+    }
+  }
+
+  /** Clears the select trigger and deselects every option in the list. */
+  private _clearSelection(skip?: GhOption): void {
+    this._selectionModel.clear();
+    this.options.forEach(option => {
+      if (option !== skip) {
+        option.deselect();
+      }
+    });
+  }
+
+  /** Emits change event to set the model value. */
+  private _propagateChanges(fallbackValue?: any): void {
+    const valueToEmit = this.selected ? (this.selected as GhOption).value : fallbackValue;
+    this._value = valueToEmit;
+    this._changeDetectiorRef.markForCheck();
   }
 }
