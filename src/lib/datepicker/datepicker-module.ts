57d18e6a51a12dffcd035920e187f6502898c233
Thomas Pink <thomas.pink@me.com>
2018-02-08 14:14:37 +0100

    feat(select): prev & next btn

--- src/lib/datepicker/datepicker-module.ts
+++ src/lib/datepicker/datepicker-module.ts
@@ -2,8 +2,10 @@ import {NgModule} from '@angular/core';
 import {GhCalendar} from './calendar';
 import {GhDatepicker} from './datepicker';
 import {GhDatepickerIntl} from './datepicker-intl';
+import {GhButtonModule} from '@dynatrace/ngx-groundhog/button';
 
 @NgModule({
+  imports: [GhButtonModule],
   exports: [
     GhCalendar,
     GhDatepicker,
