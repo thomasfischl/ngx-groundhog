bdc9eb47d8c4b6598b1dc65b9fbfcabdb77bb53d
Fabian Friedl <fabian.friedl@dynatrace.com>
2018-02-15 12:20:44 +0100

    feat(ci): add npm publish setup

---
+++ scripts/deploy/npm-publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to publish to npm.
+
+# The script should immediately exit if any command in the script fails.
+set -e
+
+echo "Starting npm publish"
+
+echo "npm publish done"
