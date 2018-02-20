94866b53e2b04eba9d58c68bf306454be11f6cd3
Thomas Pink <thomas.pink@me.com>
2018-02-20 18:28:40 +0100

    fix(core): exporting core

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -1,4 +1,5 @@
 export * from '@dynatrace/ngx-groundhog/button';
+export * from '@dynatrace/ngx-groundhog/core';
 export * from '@dynatrace/ngx-groundhog/form-field';
 export * from '@dynatrace/ngx-groundhog/icon';
 export * from '@dynatrace/ngx-groundhog/input';
