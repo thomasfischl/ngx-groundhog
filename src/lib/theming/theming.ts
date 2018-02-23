1826157984ad579d99440a49b8872e0f279f3a76
Thomas Pink <thomas.pink@me.com>
2018-02-23 12:27:40 +0100

    refactor(theming): theme directive

--- src/lib/theming/theming.ts
+++ src/lib/theming/theming.ts
@@ -1,8 +1,12 @@
 import { Directive, Input, ElementRef } from '@angular/core';
 
 const THEME_VALIDATION_RX = /([a-zA-Z-]+)(?::(light|dark))?/;
+const THEME_VARIANTS = ['light', 'dark'];
+const DEFAULT_VARIANT = 'light';
 
-export function getThemeNotValidError(name: string): Error {
+export type GhThemeVariant = 'light' | 'dark';
+
+export function getGhThemeNotValidError(name: string): Error {
   return Error(`The provided theme name "${name}" for ghTheme is not a valid theme!`);
 }
 
@@ -15,35 +19,45 @@ export function getThemeNotValidError(name: string): Error {
 })
 export class GhTheme {
 
+  /**
+   * Theme name and optional the variant.
+   * Can be:
+   * - royalblue
+   * - royalblue:light
+   * - royalblue:dark
+   */
   @Input()
   set ghTheme(value: string) {
     const result = value.match(THEME_VALIDATION_RX);
     if (result === null) {
-      throw getThemeNotValidError(value);
+      throw getGhThemeNotValidError(value);
     }
     const className = this._genClassName();
     const [, name, variant] = result;
     this._name = name;
-    if (variant) {
-      this._variant = variant as 'light' | 'dark';
-    }
+    this._variant = (THEME_VARIANTS[variant] !== -1 ? variant : DEFAULT_VARIANT) as GhThemeVariant;
     this._replaceCssClass(this._genClassName(), className);
   }
 
+  /** Name of the specified theme (royalblue, ...) */
   get name(): string | null { return this._name; }
-  get variant(): 'light' | 'dark' | null { return this._variant; }
+
+  /** Whether the theme is the light or dark variant */
+  get variant(): GhThemeVariant { return this._variant; }
 
   private _name: string | null = null;
-  private _variant: 'light' | 'dark' | null = null;
+  private _variant: GhThemeVariant = 'light';
 
   constructor(private _elementRef: ElementRef) { }
 
+  /** Generates the theme class name for the currently defined name and variant */
   private _genClassName(): string | undefined {
     const name = this._name || ``;
     const variant = this._variant ? `-${this._variant}` : '';
     return name ? `gh-theme-${name}${variant}` : undefined;
   }
 
+  /** Replaces classes on the host element */
   private _replaceCssClass(newClass?: string, oldClass?: string) {
     if (oldClass) {
       this._elementRef.nativeElement.classList.remove(oldClass);
