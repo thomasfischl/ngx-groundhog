cad6e39c4734695b49419b288dbbba874a0483a6
Thomas Pink <thomas.pink@me.com>
2018-02-27 11:09:35 +0100

    fix(theming): fixes tests

--- src/lib/theming/theming.spec.ts
+++ src/lib/theming/theming.spec.ts
@@ -20,7 +20,7 @@ describe('GhTHeme', () => {
     const sectionDebugElement = fixture.debugElement.query(By.css('section'));
     fixture.detectChanges();
 
-    expect(sectionDebugElement.nativeElement.className).toContain('gh-theme');
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme')).toBeTruthy();
   });
 
   it('should apply the corrent class if a name is set', () => {
@@ -28,19 +28,19 @@ describe('GhTHeme', () => {
     const testComponent = fixture.debugElement.componentInstance;
     const sectionDebugElement = fixture.debugElement.query(By.css('section'));
     fixture.detectChanges();
-    expect(sectionDebugElement.nativeElement.className).toContain('gh-theme-turquoise');
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-turquoise')).toBeTruthy();
 
     testComponent.theme = 'royalblue:light';
     fixture.detectChanges();
-    expect(sectionDebugElement.nativeElement.className).toContain('gh-theme-royalblue');
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-royalblue')).toBeTruthy();
 
     testComponent.theme = 'turquoise:dark';
     fixture.detectChanges();
-    expect(sectionDebugElement.nativeElement.className).toContain('gh-theme-turquoise');
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-turquoise')).toBeTruthy();
 
     testComponent.theme = ':dark';
     fixture.detectChanges();
-    expect(!(sectionDebugElement.nativeElement.className).toContain('gh-theme-turquoise'));
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-turquoise')).toBeFalsy();
   });
 
   it('should apply the corrent class if a variant is set', () => {
@@ -48,18 +48,18 @@ describe('GhTHeme', () => {
     const testComponent = fixture.debugElement.componentInstance;
     const sectionDebugElement = fixture.debugElement.query(By.css('section'));
     fixture.detectChanges();
-    expect(!(sectionDebugElement.nativeElement.className).toContain('gh-theme-light'));
-    expect(!(sectionDebugElement.nativeElement.className).toContain('gh-theme-dark'));
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-light')).toBeFalsy();
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-dark')).toBeFalsy();
 
     testComponent.theme = 'turquoise:light';
     fixture.detectChanges();
-    expect(sectionDebugElement.nativeElement.className).toContain('gh-theme-light');
-    expect(!(sectionDebugElement.nativeElement.className).toContain('gh-theme-dark'));
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-light')).toBeTruthy();
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-dark')).toBeFalsy();
 
     testComponent.theme = 'turquoise:dark';
     fixture.detectChanges();
-    expect(!(sectionDebugElement.nativeElement.className).toContain('gh-theme-light'));
-    expect(sectionDebugElement.nativeElement.className).toContain('gh-theme-dark');
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-light')).toBeFalsy();
+    expect(sectionDebugElement.nativeElement.classList.contains('gh-theme-dark')).toBeTruthy();
   });
 });
 
